f0a236802cc0d091f76806a1b5601308a2d01767
Mariana Cruz <65827628+marianafcruz17@users.noreply.github.com>
2022-04-09 04:37:02 -0300

    Create tabuleiro_xadrez.m

---
+++ tabuleiro_xadrez.m
@@ -0,0 +1,12 @@
+function xadrez = tabuleiro_xadrez()
+  tabuleiro = [ 1 0 1 0 1 0 1 0; 
+               0 1 0 1 0 1 0 1;
+               1 0 1 0 1 0 1 0; 
+               0 1 0 1 0 1 0 1;
+               1 0 1 0 1 0 1 0; 
+               0 1 0 1 0 1 0 1;
+               1 0 1 0 1 0 1 0; 
+               0 1 0 1 0 1 0 1];
+
+  imshow(tabuleiro)
+end
